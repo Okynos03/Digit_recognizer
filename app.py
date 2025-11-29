90736b6fd06e4b7793908cd40b71498ef49ed1df
Ulises Andrade <ulises.ag03@gmail.com>
2025-11-28 19:35:42 -0600

    cors fix

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
 from predict import predict_digit
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes poner tu dominio específico si quieres
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class DigitInput(BaseModel):
     pixels: list  # 28x28 = 784 valores
